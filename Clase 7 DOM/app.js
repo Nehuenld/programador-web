f6c93884c3d065a444a45a678f428b5ae0685ac9
nehuen <onamonzon.32@gmail.com>
2017-11-13 20:19:53 -0300

    git add .

---
+++ Clase 7 DOM/app.js
@@ -0,0 +1,9 @@
+var videoClub = (function () {
+    var peliculas = ["batman",
+                    "scarface",
+                    "the Matrix"]
+    var clientes = [
+    {id: 01, nombre: "Pedro Goyena"},
+    {id: 02, nombre: "Miguel Acevedo"}]
+
+})
