74e23c28dd7eda730d87f5f00720e961429c36be
nehuen <onamonzon.32@gmail.com>
2017-11-13 15:58:02 -0300

    git status

---
+++ clase 5 Arrays/ejercicio.js
@@ -0,0 +1,9 @@
+/*Crear una funcion para cargar un autos a un camion
+
+El auto tendra que tener al menos patente y peso
+El camnion tiene un peso maximo de carga
+El objeto camnion Debera validar con cada auto ingresado si esta por debajo
+ del peso o si ya no lo puede agregar
+Ademas de almacenar los autos debe tener la posibilidad de validar 
+que no tenga patentes repetidas y que me devuelva todas las patentes que tiene a bordo.*/
+
