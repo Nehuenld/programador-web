9785898ed63a5124284b011626b8edb938fcd22f
nehuen <onamonzon.32@gmail.com>
2017-11-13 21:03:11 -0300

    git commit add .

--- Clase 7 DOM/app.js
+++ Clase 7 DOM/app.js
@@ -1,9 +1,4 @@
-var videoClub = (function () {
-    var peliculas = ["batman",
-                    "scarface",
-                    "the Matrix"]
-    var clientes = [
-    {id: 01, nombre: "Pedro Goyena"},
-    {id: 02, nombre: "Miguel Acevedo"}]
-
-})
+/*Nos sirve para modificar los elemntos que 
+definimos en nuestro html
+*/
+var sectionTitle = 
